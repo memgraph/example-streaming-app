54d93799e43745250a0a28b0497a54e8fa68ce34
MasterMedo <mislav.vuletic@gmail.com>
2021-06-27 10:53:18 +0200

    add logging and reorganize code

--- backend/python/app.py
+++ backend/python/app.py
@@ -10,41 +10,69 @@ edge_type - a string: `CONNECTED_WITH`
 fields - a string in form of a json/python dictionary:
     `{age: 53}` or `{id: 4, name: "hero", alive: true}`
 """
+import csv
+import logging
+
 from gqlalchemy import Memgraph
 from kafka import KafkaConsumer
 
 
-db = Memgraph()
-# db.drop_database()
-consumer = KafkaConsumer(
-    'topic',
-    bootstrap_servers=['localhost:9092']
-)
+def prepare(message):
+    return message.value.decode('utf-8')
 
-for message in consumer:
+
+def process(message, db):
+    command, *payload = message
+
+    if command == 'node':
+        label, unique_fields, fields = payload
+        db.execute_query(
+            f'merge (a:{label} {unique_fields}) '
+            f'set a += {fields}'
+        )
+    elif command == 'edge':
+        (
+            label1,
+            unique_fields1,
+            edge_type,
+            edge_fields,
+            label2,
+            unique_fields2,
+        ) = payload
+        db.execute_query(
+            f'merge (a:{label1} {unique_fields1}) '
+            f'merge (b:{label2} {unique_fields2}) '
+            f'merge (a)-[:{edge_type} {edge_fields}]->(b)'
+        )
+    else:
+        raise ValueError(f'Command `{command}` not recognized.')
+
+    logging.info(
+        '`' + '|'.join(message)
+        + '`, Successfully entered {command} in Memgraph.'
+    )
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        filename='info.log',
+        encoding='utf-8',
+        level=logging.INFO,
+        format='%(levelname)s: %(asctime)s %(message)s'
+    )
+    db = Memgraph()
+    # db.drop_database()
+    consumer = KafkaConsumer(
+        'topic',
+        bootstrap_servers=['localhost:9092']
+    )
     try:
-        message = message.value.decode('utf-8')
-        payload = message.split('|')
-        command = payload.pop(0)
+        for message in csv.reader(map(prepare, consumer), delimiter='|'):
+            try:
+                process(message, db)
+            except Exception as error:
+                logging.error('`' + '|'.join(message) + '`, ' + repr(error))
+                continue
 
-        if command == 'node':
-            label, unique_fields, fields = payload
-            db.execute_query(f'merge (a:{label} {unique_fields}) '
-                             f'set a += {fields}')
-        elif command == 'edge':
-            (
-                label1,
-                unique_fields1,
-                edge_type,
-                edge_fields,
-                label2,
-                unique_fields2,
-            ) = payload
-            db.execute_query(f'merge (a:{label1} {unique_fields1}) '
-                             f'merge (b:{label2} {unique_fields2}) '
-                             f'merge (a)-[:{edge_type} {edge_fields}]->(b)')
-    except Exception as error:
-        print('Error: A faulty message was passed to the consumer:')
-        print(f'{message=}')
-        print(f'{error=}')
-        continue
+    except KeyboardInterrupt:
+        pass
