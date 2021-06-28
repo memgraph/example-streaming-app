647aaeee0af84fb180bc19f602fc648ef1b5f71f
Marko Budiselic <marko.budiselic@memgraph.com>
2021-06-28 14:18:42 +0200

    Add memgraph_binary_path to the start script

--- backend/node/scripts/memgraph/start.sh
+++ backend/node/scripts/memgraph/start.sh
@@ -3,8 +3,9 @@
 # TODO(gitbuda): Replace Memgraph start script with something any user can start.
 
 help_and_exit () {
-    echo "USAGE: $0 memgraph|action"
+    echo "USAGE: $0 memgraph|action [memgraph_binary_path]"
     echo "    where action is a filename (excluding extension) from queries dir."
+    echo "    default memgraph_binary_path is /usr/lib/memgraph/memgraph"
     exit 1
 }
 
@@ -12,10 +13,14 @@ set -Eeuo pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$script_dir" || help_and_exit
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 1 ] && [ "$#" -ne 2 ]; then
     help_and_exit
 fi
 action="$1"
+memgraph_binary_path="/usr/lib/memgraph/memgraph"
+if [ "$#" -eq 2 ]; then
+    memgraph_binary_path="$2"
+fi
 
 if [ -f "$script_dir/queries/$action.gql" ]; then
     cat < "$script_dir/queries/$action.gql" | mgconsole
@@ -24,7 +29,7 @@ fi
 
 case "$action" in
     memgraph)
-        sudo runuser -l memgraph -c "/usr/lib/memgraph/memgraph --query-modules-directory=$script_dir/query_modules --log-level=DEBUG --also-log-to-stderr"
+        sudo runuser -l memgraph -c "$memgraph_binary_path --query-modules-directory=$script_dir/query_modules --log-level=DEBUG --also-log-to-stderr"
     ;;
 
     *)
