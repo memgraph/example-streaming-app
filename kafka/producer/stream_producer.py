f610eae260656b19d427b4190d34a85da4287761
MasterMedo <mislav.vuletic@gmail.com>
2021-07-01 09:29:39 +0200

    update stream producer

--- kafka/producer/stream_producer.py
+++ kafka/producer/stream_producer.py
@@ -14,14 +14,19 @@ for i in count():
     fields1 = f'{{id: {i}}}'
     for j in random.sample(range(0, i), random.randint(0, min(i, 20))):
         fields2 = f'{{id: {j}}}'
-        producer.send(
-            f'{label1}|{fields1}|{edge_type}|{edge_fields}|{label2}|{fields2}'
-        )
-        producer.send(
-            f'{label2}|{fields2}|{edge_type}|{edge_fields}|{label1}|{fields1}'
-        )
-with open('input.csv') as f:
-    for line in f.read().split('\n')[:-1]:
-        producer.send('topic', line.encode('utf-8'))
-        producer.flush()
-        sleep(0.1)
+        messages = [
+            f'edge|{label1}|{fields1}|{edge_type}|{edge_fields}|{label2}|{fields2}',
+            f'edge|{label2}|{fields2}|{edge_type}|{edge_fields}|{label1}|{fields1}',
+        ]
+        for message in messages:
+            print(message)
+            producer.send(
+                'topic',
+                message.encode('utf-8')
+            )
+    producer.send(
+        'topic',
+        f'node|{label1}|{fields1}|{{name: {"person"}}}'.encode('utf-8')
+    )
+    producer.flush()
+    sleep(4)
