943c541012a6756cd60ffda63401ed572d75c3aa
MasterMedo <mislav.vuletic@gmail.com>
2021-07-07 10:57:35 +0200

    add kafka

--- backend/go/app.go
+++ backend/go/app.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"strings"
 
 	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
+	"github.com/segmentio/kafka-go"
 )
 
 func main() {
@@ -18,24 +20,40 @@ func main() {
 	cypherNodeCommand := "MERGE (node:%s %s) " +
 		"SET node += %s"
 	cypherEdgeCommand := "MERGE (node1:%s %s)" +
-		"MERGE (node2:%s %s" +
+		"MERGE (node2:%s %s) " +
 		"MERGE (node1)-[:%s %s]->(node2)"
 
-	message := "node|Person|{id: 0}|{name: 'ahoy'}"
-	cypherCommand := ""
-	arr := strings.Split(message, "|")
+	kafkaReader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:  []string{"localhost:9092"},
+		Topic:    "topic",
+		MinBytes: 0,
+		MaxBytes: 10e6,
+	})
+	defer kafkaReader.Close()
+	for {
+		kafkaMessage, err := kafkaReader.ReadMessage(context.Background())
+		if err != nil {
+			fmt.Println("nothing to read...")
+			break
+		}
+		message := string(kafkaMessage.Value)
+		cypherCommand := ""
+		arr := strings.Split(message, "|")
 
-	switch arr[0] {
-	case "node":
-		cypherCommand = fmt.Sprintf(cypherNodeCommand, arr[1], arr[2], arr[3])
-	case "edge":
-		cypherCommand = fmt.Sprintf(cypherEdgeCommand, arr[1], arr[2], arr[5], arr[6], arr[3], arr[4])
-	default:
-		panic(err)
-	}
-	err = runCypherCommand(driver, cypherCommand)
-	if err != nil {
-		panic(err)
+		switch arr[0] {
+		case "node":
+			cypherCommand = fmt.Sprintf(cypherNodeCommand, arr[1], arr[2], arr[3])
+		case "edge":
+			cypherCommand = fmt.Sprintf(cypherEdgeCommand, arr[1], arr[2], arr[5], arr[6], arr[3], arr[4])
+		}
+		if cypherCommand == "" {
+			fmt.Println("breaking")
+			break
+		}
+		err = runCypherCommand(driver, cypherCommand)
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
