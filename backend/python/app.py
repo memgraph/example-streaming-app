76352a387a8b26823239babd69d502e798705757
MasterMedo <mislav.vuletic@gmail.com>
2021-07-08 12:05:24 +0200

    python print number of neighbors

--- backend/python/app.py
+++ backend/python/app.py
@@ -34,16 +34,23 @@ def process(message: str, db: Memgraph):
 
     Throws a ValueError if the command isn't recognised.
     """
-    payload = next(csv.reader([message], delimiter='|'))
+    payload = next(csv.reader([message], delimiter="|"))
     command, *payload = payload
 
-    if command == 'node':
+    if command == "node":
         label, unique_fields, fields = payload
-        db.execute_query(
-            f'merge (a:{label} {unique_fields}) '
-            f'set a += {fields}'
-        )
-    elif command == 'edge':
+        db.execute_query(f"merge (a:{label} {unique_fields}) set a += {fields}")
+        neighbors = next(db.execute_and_fetch(
+            f"match (a:{label} {unique_fields}) return a.neighbors as n"
+        ))['n']
+        if neighbors is None:
+            print(
+                "The neighbors variable isn't set. "
+                "Memgraph triggers are probably not set up properly."
+            )
+        else:
+            print(f"(node:{label} {unique_fields}) has {neighbors} neighbors.")
+    elif command == "edge":
         (
             label1,
             unique_fields1,
@@ -53,35 +60,32 @@ def process(message: str, db: Memgraph):
             unique_fields2,
         ) = payload
         db.execute_query(
-            f'merge (a:{label1} {unique_fields1}) '
-            f'merge (b:{label2} {unique_fields2}) '
-            f'merge (a)-[:{edge_type} {edge_fields}]->(b)'
+            f"merge (a:{label1} {unique_fields1}) "
+            f"merge (b:{label2} {unique_fields2}) "
+            f"merge (a)-[:{edge_type} {edge_fields}]->(b)"
         )
     else:
-        raise ValueError(f'Command `{command}` not recognized.')
-    logging.info(f'`{message}`, Successfully entered {command} in Memgraph.')
+        raise ValueError(f"Command `{command}` not recognized.")
+    logging.info(f"`{message}`, Successfully entered {command} in Memgraph.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(
-        filename='info.log',
-        encoding='utf-8',
+        filename="info.log",
+        encoding="utf-8",
         level=logging.INFO,
-        format='%(levelname)s: %(asctime)s %(message)s'
+        format="%(levelname)s: %(asctime)s %(message)s",
     )
-    db = Memgraph(host='localhost', port=7687)
+    db = Memgraph(host="localhost", port=7687)
     db.drop_database()
-    consumer = KafkaConsumer(
-        'topic',
-        bootstrap_servers=['localhost:9092']
-    )
+    consumer = KafkaConsumer("topic", bootstrap_servers=["localhost:9092"])
     try:
         for message in consumer:
-            message = message.value.decode('utf-8')
+            message = message.value.decode("utf-8")
             try:
                 process(message, db)
             except Exception as error:
-                logging.error(f'`{message}`, {repr(error)}')
+                logging.error(f"`{message}`, {repr(error)}")
                 continue
 
     except KeyboardInterrupt:
